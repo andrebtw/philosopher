264f4bb61e141590cf1d8c3405f03017c1d843f1
andrebtw <gwandrewgd@gmail.com>
2023-08-08 20:28:30 +0200

    Thread created. But they leak.

--- philo/incl/philo.h
+++ philo/incl/philo.h
@@ -6,7 +6,7 @@
 /*   By: anrodri2 <anrodri2@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 05:34:39 by anrodri2          #+#    #+#             */
-/*   Updated: 2023/08/07 08:31:11 by anrodri2         ###   ########.fr       */
+/*   Updated: 2023/08/08 20:15:03 by anrodri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <pthread.h>
 
 /* --- Boolean macros --- */
 # define TRUE 1
@@ -40,6 +41,8 @@
 /* --- Error messages --- */
 # define ERR_MALLOC_MSG "Memory allocation has failed. \x1B[0m\n\x1B[33mPlease\
 make sure enough memory is dedicated to the process.\n"
+# define ERR_THREAD_MSG "Thread init has failed. \x1B[0m\n\x1B[33mPlease\
+make sure enough resources are available.\n"
 # define ERR_WRONG_ARGS_MSG "⛔\x1B[33m: Wrong number of arguments.\x1B[37m\n"
 # define ERR_NOT_DIGIT "⛔\x1B[33m: Please make sure to \
 only send digits.\x1B[37m\n"
@@ -52,6 +55,26 @@ at least send 1 philo.\x1B[37m\n"
 
 /* --- Special codes --- */
 # define NOT_INIT -15
+# define ERR_MEM_ALLOC_FAILED 12
+# define ERR_THREAD_FAILED 13
+
+
+/* --- Main struct --- */
+typedef struct s_thread
+{
+	int	thread_id;
+} t_thread ;
+
+typedef struct s_philo
+{
+	pthread_t	*threads;
+	t_thread	thread;
+	int			philo_count;
+	int			time_to_eat;
+	int			time_to_sleep;
+	int			time_to_die;
+	int			must_eat;	
+} t_philo ;
 
 /* --- Errors in parsing --- */
 int	check_for_errors(int argc, char **argv);
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,15 +6,59 @@
 /*   By: anrodri2 <anrodri2@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 05:34:05 by anrodri2          #+#    #+#             */
-/*   Updated: 2023/07/28 16:23:01 by anrodri2         ###   ########.fr       */
+/*   Updated: 2023/08/08 20:27:52 by anrodri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	init(t_philo *philo, char **argv)
+{
+	philo->philo_count = ft_atoi(argv[1]);
+	philo->time_to_die = ft_atoi(argv[2]);
+	philo->time_to_eat = ft_atoi(argv[3]);
+	philo->time_to_sleep = ft_atoi(argv[4]);
+	if (argv[5])
+		philo->must_eat = ft_atoi(argv[5]);
+}
+
+void	*func(void *args)
+{
+	(void)args;
+	printf("PHILO CREATED!!!\n");
+	pthread_exit(NULL);
+}
+
+int	threads_init(t_philo *philo)
+{
+	long	i;
+
+	i = 0;
+	philo->threads = malloc(sizeof(pthread_t) * philo->philo_count + 1);
+	while (i < philo->philo_count)
+	{
+		if (pthread_create(&philo->threads[i], NULL, &func, NULL) != 0)
+			return (free(philo->threads), ERR_THREAD_FAILED);
+		i++;
+	}
+	return (EXIT_SUCCESS);
+}
+
 int	main(int argc, char **argv)
 {
+	t_philo	philo;
+	int		ret_value;
+
 	if (check_for_errors(argc - 1, argv))
 		return (EXIT_FAILURE);
+	init(&philo, argv);	
+	ret_value = threads_init(&philo);
+	if (ret_value == ERR_MEM_ALLOC_FAILED)
+		return (ERR_MEM_ALLOC_FAILED);
+	else if (ret_value == ERR_THREAD_FAILED)
+	{
+		ft_putstr_fd(ERR_THREAD_MSG, STDERR_FILENO);
+		return (ERR_THREAD_FAILED);
+	}
 	return (EXIT_SUCCESS);
 }
